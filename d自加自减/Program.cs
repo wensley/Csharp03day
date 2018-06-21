17a298ef440302b6025d44fa78adbc52dbd8f106
ailao <ailao@IDEA-PC>
2018-06-21 22:04:09 +0800

    d自加自减

---
+++ d自加自减/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d自加自减
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int age=18
+            //int sum = ++age - 10;
+            //Console.WriteLine("age={0}",age);
+            //Console.WriteLine("sum={0}",sum);
+
+
+            //int age = 18;
+            //age += 3;  //等同于age=age+3;
+            //age -= 3;
+            //Console.WriteLine("age={0}",age);
+
+            //int zsAge = 20;
+            //int lsAge = 18;
+            //bool isRight = zsAge > lsAge;
+            //Console.WriteLine(isRight);
+
+            //bool练习
+            //int xiang = 1500, shu = 1;
+            //bool isRight = xiang > shu;
+            //Console.WriteLine(isRight);
+
+            //int tuzi = 3, wugui = 1000;
+            //bool isRight = tuzi > wugui;
+            //Console.WriteLine(isRight);
+
+            int my = 20, you = 20;
+            bool isRight = my == you;
+            Console.WriteLine(isRight);
+
+            Console.ReadKey();
+        }
+    }
+}
