f79f35e2f5a1433dd078f08240fd81efd250d83a
ailao <ailao@IDEA-PC>
2018-06-25 21:41:01 +0800

    09if讲解及练习作业

---
+++ 09if/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if的基本用法
+            //int a = 3;
+            //if (a>10)
+            //    Console.WriteLine("成立");//if默认织带这一句话
+            //Console.WriteLine("程序结束");//这句话和if没有关系
+
+            //Console.WriteLine("请输入你的分数：");
+            //int zs = Convert.ToInt32(Console.ReadLine());
+            //if (zs > 90)
+            //    Console.WriteLine("爸爸奖励你100元！");
+            //Console.WriteLine("程序结束");
+
+            ////作业1
+            //Console.WriteLine("请输入你的年龄：");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //if (age>=18)
+            //    Console.WriteLine("你已成年！");
+            //Console.WriteLine("程序结束！");
+
+            //作业2-不会做
+            int chinese=90, music=70;
+            if  (chinese > 90 && music = 80 || chinese = 100 && music > 70);
+            {
+                Console.WriteLine();
+            }
+            
+            Console.ReadKey();
+
+                //作业3 不会做，完全懵逼了
+                Console.WriteLine(a.ToString() + b);
+
+
+            Console.WriteLine("请输入用户名：");
+            string user = Console.ReadLine();
+            Console.WriteLine("请输入密码：");
+            string pass = Console.ReadLine();
+            if( user=user  pass= pass)
+
+                Console.WriteLine("登陆成功");
+            Console.WriteLine("登陆失败");
+
+
+            Console.ReadKey();
+        }
+    }
+}
